0995006f719be2becd4f9dd09fa2afd844c95491
jadoucis <jade.grau@epsi.fr>
2023-02-22 13:20:13 +0100

    first interface

--- DjangoReservation/settings.py
+++ DjangoReservation/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'DjangoReservation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'pilotageSchool'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- pilotageSchool/views.py
+++ pilotageSchool/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import School
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world.")
+    schools = School.objects.all()
+    return render(request, 'templates/school_list.html', {'schools': schools})
