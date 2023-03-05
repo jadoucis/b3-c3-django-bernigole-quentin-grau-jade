6a29b91b98ed84ccfd4f9eb5fad2cc71edbf054f
SM9LE <quentin.bernigole@hotmail.com>
2023-03-05 01:34:29 +0100

    Adding login feature and informative messages

--- pilotageSchool/views.py
+++ pilotageSchool/views.py
@@ -16,3 +16,24 @@ def index(request):
 # Register view (Bonus)
 
 # Login view
+def login_page(request):
+    form = forms.LoginForm()
+    if request.user.is_authenticated:
+        return redirect("index")
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Bonjour {user.username}, vous êtes connecté !")
+            else:
+                messages.info(request, f"Echec de la connexion")
+    return render(request, 'login.html', context={'form' : form})
+
+# Logout view
+@login_required
+def logout_page(request):
+    logout(request)
+    messages.info(request, "Vous êtes déconnecté.")
+    return redirect("index")
