e33f874645a6b16f54f6a2551f7357143a46c924
SM9LE <quentin.bernigole@hotmail.com>
2023-03-05 12:12:03 +0100

    Adding placeholder on registration page

--- pilotageSchool/forms.py
+++ pilotageSchool/forms.py
@@ -13,11 +13,11 @@ class RegisterForm(UserCreationForm):
 
     username = forms.CharField(max_length=200, label='Nom d\'utilisateur', widget=forms.TextInput(attrs={'class': 'form-control form-control-lg', 'placeholder':'Rida123'}),)
     email = forms.EmailField(label='Adresse mail', widget=forms.EmailInput(
-        attrs={'class': 'form-control form-control-lg', 'placeholder': 'example@gmail.com'}))
+        attrs={'class': 'form-control form-control-lg', 'placeholder': 'rida.messaoudene@reseau-cd.fr'}))
     first_name = forms.CharField(label='Prénom', max_length=50,
-                                 widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}))
+                                 widget=forms.TextInput(attrs={'class': 'form-control form-control-lg', 'placeholder' : 'Rida'}))
     last_name = forms.CharField(label='Nom de famille', max_length=50,
-                                widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}))
+                                widget=forms.TextInput(attrs={'class': 'form-control form-control-lg', 'placeholder' : 'Messaoudene'}))
 
     class Meta:
         model = User
