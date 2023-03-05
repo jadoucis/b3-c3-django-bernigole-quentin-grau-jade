4ae40bd81fb7ddb8085ec245b989046ae8b382dc
SM9LE <quentin.bernigole@hotmail.com>
2023-03-05 01:21:26 +0100

    Adding a Navbar and index page

--- pilotageSchool/views.py
+++ pilotageSchool/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django import forms
+from . import forms
 
 # Create your views here.
+
+# Main page view
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request, "index.html")
+
+
+# Register view (Bonus)
+
+# Login view
