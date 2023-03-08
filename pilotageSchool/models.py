8d7100fa4e44c13fd0a9d8ce81950b7c3eabaea9
jadoucis <jade.grau@epsi.fr>
2023-03-08 16:53:34 +0100

    adjust models

--- pilotageSchool/models.py
+++ pilotageSchool/models.py
@@ -1,5 +1,7 @@
 import datetime
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
 class School(models.Model):
@@ -7,16 +9,27 @@ class School(models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     phone = models.CharField(max_length=10)
+
     def __str__(self):
         return f'Nom de l\'école : {self.name} - Adresse : {self.address}'
 
+
 class Reservation(models.Model):
-    School = models.ForeignKey(School, on_delete=models.CASCADE)
-    className = models.CharField(max_length=200)
+    TIME_CHOICES = (
+        ("8h", "8h-9h"),
+        ("9h", "9h-10h"),
+        ("10h", "10h-11h"),
+        ("11h", "11h-12h"),
+        ("14h", "14h-15h"),
+        ("15h", "15h-16h"),
+        ("16h", "16h-17h"),
+        ("17h", "17h-18h"),
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
     description = models.CharField(max_length=200)
     date = models.DateField()
-    heureDebut = models.TimeField()
-    heureFin = models.TimeField()
-    isReserved = models.BooleanField(default=False)
+    time = models.CharField(max_length=50, choices=TIME_CHOICES)
+
     def __str__(self):
-        return f'Nom de l\'école : {self.schoolName} - Intitulé du cours : {self.className} - Date du cours : {self.date} - Heure début cours : {self.heureDebut} - Est-il réservé : {self.isReserved}'
+        return f'{self.user} a réservé un créneau le {self.date} à {self.time} pour l école {self.school}'
